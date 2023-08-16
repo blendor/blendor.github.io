771ba65d344f2653766f7d6691200e57c7c97780
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-08-16 18:55:50 +0200

    styling & content - frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { bounceInOut } from "svelte/easing";
+
+</script>
+
+<div class="center large">
+	<h1>Hi, I'm Blendor! 👋</h1>
+</div>
+<div>
+	<h2>I do a lot of things</h2>
+	<p>Including:</p>
+</div>
+<div class="bento-big">
+    <div class="bento-box">
+
+    </div>
+    <div class="container">
+        <h3><b>Digital products</b></h3>
+			<p>As a cofounder of digital product studio <a href="https://cloverlabs.io">Clover Labs</a></p>
+    </div>
+
+</div>
+
+<style>
+</style>
