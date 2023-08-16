d881b3e61c02f0e6743b50553d659c614c42ff1f
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-08-16 18:37:36 +0200

    create links subpage, GA4 component, add GA4 on layout

---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    // your script goes here
+</script>
+
+<h1>Links</h1>
+<h2></h2>
+<ul>
+    <li><a href="https://blendorsefaj.com/">Website</a></li>
+    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+    <li><a href="https://linkedin.com/in/blendor">LinkedIn</a></li>
+    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+
+</ul>
+
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
