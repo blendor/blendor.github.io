5f471f03344bad168443c0b9f99915ccf90e0b48
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-08-16 18:38:18 +0200

    add blog main page

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/now">Now</a>
+    <a href="{base}/links">Links</a>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Navbar from '../lib/Navbar.svelte';
+	import Analytics from '../lib/analytics.svelte';
+</script>
+
+<Analytics />
+<Navbar />
+<slot />
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+	/* your styles go here */
+</style>
