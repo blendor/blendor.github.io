42bff2975a9e4e6a260240dacc8dc2279e6ca554
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-08-16 14:43:53 +0200

    move nojekyll to root, modify layout.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,9 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter(
+			{fallback: '200.html'}
+		),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/blendor.github.io' : '',
         }
