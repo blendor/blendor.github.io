6733d493a547bf58959df655f447030fba922d16
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-08-16 14:34:19 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	// your script goes here
+
+	import Navbar from '../lib/Navbar.svelte';
+</script>
+
+<Navbar />
+<slot />
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+	/* your styles go here */
+</style>
---
+++ src/routes/now/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	// your script goes here
+</script>
+
+<h1>Now</h1>
+<p>Test</p>
+
+<!-- markup (zero or more items) goes here -->
+
+<style>
+	/* your styles go here */
+</style>
