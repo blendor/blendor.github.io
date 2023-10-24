08e09d0e58745de04963ada93bf0c5243945034a
Blendor Sefaj <blendorsefaj1@gmail.com>
2023-10-24 16:23:48 +0200

    addition

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -2,40 +2,8 @@
 </script>
 
 <slot />
-<div>
-	<div class="card">
-		<img
-			src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-			alt="Avatar"
-			style="width:100%"
-		/>
-		<div class="container">
-			<h4><b>John Doe</b></h4>
-			<p>Architect & Engineer</p>
-		</div>
-	</div>
-</div>
 
 <!-- markup (zero or more items) goes here -->
 
 <style>
-	img {
-		height: 300px;
-		width: 200px;
-	}
-	.card {
-		/* Add shadows to create the "card" effect */
-		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-		transition: 0.3s;
-	}
-
-	/* On mouse-over, add a deeper shadow */
-	.card:hover {
-		box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-	}
-
-	/* Add some padding inside the card container */
-	.container {
-		padding: 2px 16px;
-	}
 </style>
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,22 +1,39 @@
 <script>
-    // your script goes here
+	// your script goes here
 </script>
 
 <h1>Links</h1>
-<h2></h2>
-<ul>
-    <li><a href="https://blendorsefaj.com/">Website</a></li>
-    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
-    <li><a href="https://linkedin.com/in/blendor">LinkedIn</a></li>
-    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
-    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
-    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
-    <li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+<p>This is the place, where I store my links:</p>
 
+<h2>Me</h2>
+<ul>
+	<li><a href="https://blendorsefaj.com/">Website</a></li>
+	<li><a href="https://twitter.com/blendorsefaj">Twitter</a></li>
+	<li><a href="https://linkedin.com/in/blendor">LinkedIn</a></li>
 </ul>
 
-<style>
-    /* your styles go here */
-</style>
+<h2>What I think is great to:</h2>
+
+<h3>Read</h3>
+
+<ul>
+	<li>
+		<a href="https://www.goodreads.com/book/show/447648.Getting_Real"
+			>Getting Real: The Smarter, Faster, Easier Way to Build a Web Application</a
+		>
+	</li>
+</ul>
+
+<h3>Watch</h3>
+
+<ul>
+	<li>
+		<a href="https://youtu.be/qHj8fd-034s?">Jerry Seinfeld: What does it take to be a comedian?</a>
+	</li>
+</ul>
 
 <!-- markup (zero or more items) goes here -->
+
+<style>
+	/* your styles go here */
+</style>
